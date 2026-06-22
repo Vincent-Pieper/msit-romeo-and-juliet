cd657b7007e1f00a29f988bbda3df743b4d470d1
Vincent-Pieper <pieper.vincent@gmail.com>
2026-06-22 15:57:33 +0200

    implement word frequency analysis and top word output

--- frequency.py
+++ frequency.py
@@ -10,11 +10,10 @@ def words_frequency(basic_words):
     unique_words = get_unique_words(basic_words)
     no_count_dict = create_dict(unique_words)
     count_dict = counted_list(no_count_dict, basic_words)
-    print(count_dict)
-    #continue Step 3 on cosio
+    return count_dict
 
 
-def get_unique_words (basic_words):
+def get_unique_words(basic_words):
     unique_words = []
     for word in basic_words:
         if word in unique_words:
@@ -24,7 +23,7 @@ def get_unique_words (basic_words):
     return unique_words
 
 
-def create_dict (unique_words):
+def create_dict(unique_words):
     no_count_dict = {}
     for word in unique_words:
         no_count_dict[word] = 0
@@ -37,11 +36,21 @@ def counted_list(no_count_dict, basic_words):
     return no_count_dict
 
 
+def top_n_words(freq, n):
+    working_freq = freq.copy()
+    for i in range(n):
+        most_common_word = max(working_freq, key=working_freq.get)
+        most_common_value = working_freq[most_common_word]
+        print(f"{most_common_word}: {most_common_value}")
+        del working_freq[most_common_word]
 
 
 def main():
     basic_words = get_words(PLAY)
-    words_frequency(basic_words)
+    freq = words_frequency(basic_words)
+    n = int(input("How many words should be shown? "))
+    top_n_words(freq, n)
+
 
 if __name__ == "__main__":
     main()
